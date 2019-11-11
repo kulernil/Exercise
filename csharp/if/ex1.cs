cf1484ae23cdef42a4ec1f3d1848615fee61cacf
kulernil <kuler@vivaldi.net>
2019-11-11 11:31:19 +0800

    Add solution ex1 to for statement.

---
+++ csharp/if/ex1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,y,z;
+            double average, product;
+
+            x = Int32.Parse(Console.ReadLine());
+            y = Int32.Parse(Console.ReadLine());
+            z = Int32.Parse(Console.ReadLine());
+
+            average = ( x + y + z ) / 2;
+            product =  x * y * z;
+
+            Console.WriteLine("The average of numbers is " + average);
+            Console.WriteLine("The product of numbers is " + product);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
