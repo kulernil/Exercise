7907fde08d15b3cd0654a5a115ff123f72d227cb
kulernil <kuler@vivaldi.net>
2019-11-11 11:44:37 +0800

    Add solution ex2 to if statement.

---
+++ csharp/if/ex2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int celsius;
+            double farenheit;
+
+            Console.WriteLine("Please input degree in celsius.");
+            celsius = Int32.Parse(Console.ReadLine());
+            farenheit = 9/5 * celsius + 32;
+
+            Console.WriteLine("Degree in farenheit is " + farenheit);
+        }
+    }
+}
