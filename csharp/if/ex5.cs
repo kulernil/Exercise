786646c56aaca46c8a9ec46574bebc383ead135b
kulernil <kuler@vivaldi.net>
2019-11-11 20:31:45 +0800

    Add solution ex5 to if statement

---
+++ csharp/if/ex5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int hundreds, tens, ones;
+
+            n = Int32.Parse(Console.ReadLine());
+            
+            hundreds = n / 100;
+            tens = ( n % 100 ) / 10;
+            ones = ( n % 10 );
+
+            Console.Write(ones);
+            Console.Write(tens);
+            Console.Write(hundreds);
+
+        }
+    }
+}
