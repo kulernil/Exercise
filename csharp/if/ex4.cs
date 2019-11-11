ef88eaaa0918db06563016bad6c0b3da0db2d6ea
kulernil <kuler@vivaldi.net>
2019-11-11 20:22:59 +0800

    Add solution ex4 to if statement

---
+++ csharp/if/ex4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char c1 = s[0];
+            char c2 = s[1];
+            char c3 = s[2];
+            Console.Write(c3);
+            Console.Write(c2);
+            Console.Write(c1);
+        }
+    }
+}
