7df9594e8a566a6df75c74c78286f76279bc68ed
kulernil <kuler@vivaldi.net>
2019-11-11 11:57:47 +0800

    Add solution ex3 to if statement

---
+++ csharp/if/ex3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length, width, height;
+            double volume, area;
+            
+            Console.WriteLine("Please insert length, width and height:\n");
+            length = Int32.Parse(Console.ReadLine());
+            width  = Int32.Parse(Console.ReadLine());
+            height = Int32.Parse(Console.ReadLine());
+
+            volume = length * width * height;
+            area = ( length * width + length * height + width * height ) * 2;
+
+            Console.WriteLine($"The volume is {volume} and area is {area}.");
+
+        }
+    }
+}
