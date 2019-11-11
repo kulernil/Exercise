f1b74cbb379a6edc2b69e195598b0ec2fd7713ef
kulernil <kuler@vivaldi.net>
2019-11-11 20:54:43 +0800

    Add solution ex6 to if statement

---
+++ csharp/if/ex6.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char c1 = s[0];
+            char c2 = s[1];
+            char c3 = s[2];
+
+            int a, b, c;
+            a = c1 - c2;
+            b = c2 - c3;
+            c = c1 - c3;
+            
+            if( (( a == 1 ) || ( a == -1 )) ||
+                (( b == 1 ) || ( b == -1 )) ||
+                (( c == 1 ) || ( c == -1 )) )
+                {
+                    Console.Write("TRUE");
+                }
+            else
+                {
+                    Console.Write("FALSE"); 
+                }
+        }
+    }
+}
