26c11c02b0fd535b0cc8bda72ed4d5a1da7acae0
Daniel Young <young@debian>
2019-11-21 01:12:59 -0500

    Add solution ex2 to example for

---
+++ csharp/for/ex2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int i = 0;
+             while( n < 100000 )
+                {
+                    if ((Math.Truncate(Math.Sqrt(n+100)) == (Math.Sqrt(n+100))) && (Math.Truncate(Math.Sqrt(n+268)) != Math.Sqrt(n+268)))
+                        {
+                            Console.WriteLine(n);
+                            i++;
+                            };
+                    n++;
+                } 
+            Console.WriteLine(i);
+        }
+    }
+}
