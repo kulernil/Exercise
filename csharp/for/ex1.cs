73e9fd25923e4b6bfba915ff8816c2a5b67eaaf8
Daniel Young <young@debian>
2019-11-20 22:35:42 -0500

    Add solution ex1 to for example

---
+++ csharp/for/ex1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            for(int i=1; i<=4; i++)
+                for(int j=1; j<=4; j++)
+                    for(int k=1; k<=4; k++)
+                        if(i!=j && j!=k && i!=k)
+                            {
+                                Console.WriteLine(i*100 + j*10 + k);
+                                n++;
+                            }
+            Console.WriteLine(n);
+
+        }
+    }
+}
