dbeb23fa83ed891abaa7ae77f4db6f88b1e8a1e9
Daniel Young <young@debian>
2019-11-22 11:02:49 -0500

    Add solution ex4 to exmaple for

---
+++ csharp/for/ex4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=2;
+            
+            for(int i=101; i<200; i++)
+            {
+                while(n<=Math.Round(Math.Sqrt(i)))
+                {
+                    if( i%n==0 ) break;
+                    n++;
+                }
+                if (n > Math.Round(Math.Sqrt(i)))
+                    Console.WriteLine(i);
+                n=2;
+            }
+
+
+        }
+    }
+}
