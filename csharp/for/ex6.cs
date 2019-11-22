65e624fe0c4156ecb81a7d848b353efa60aea25a
Daniel Young <young@debian>
2019-11-22 11:27:46 -0500

    Add solution ex6 to for exmaple

---
+++ csharp/for/ex6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float i, j;
+            float tmp=0;
+            i=1; j=2;
+
+            Console.Write("tmp=");
+            for(int k=1; k<=20; k++)
+            {
+                tmp += j/i;
+                Console.Write((k == 20) ? $"{j}/{i}" : $"{j}/{i}+");
+                j = j + i;
+                i = j - i;         
+            }
+            Console.WriteLine();
+            Console.WriteLine("="+tmp);
+        }
+    }
+}
