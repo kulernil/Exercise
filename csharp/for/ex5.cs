b79ed269742df47b1df9ac1738163770bd451dfd
Daniel Young <young@debian>
2019-11-22 11:12:39 -0500

    Add solution ex5 to example for

---
+++ csharp/for/ex5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int times=1;
+            float origin_height= 100;
+
+            while(times < 10)
+            {
+                Console.WriteLine("{0}:\t{1}m", times, origin_height);
+                times++;
+                origin_height /= 2;
+            }
+        }
+    }
+}
