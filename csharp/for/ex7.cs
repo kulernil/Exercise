51f7fbb581e75a403d736c8c7e86caa8abbe0a24
Daniel Young <young@debian>
2019-11-22 11:37:35 -0500

    Add solution ex7 to for example

---
+++ csharp/for/ex7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   int total=0;
+            for(int n=2; n<=100; n++)
+            {
+                int factor=2;
+                while (factor <= (Math.Sqrt(n)))
+                {
+                    if (n%factor == 0) break;
+                    factor++;
+                }
+                if(factor > Math.Sqrt(n))
+                {
+                    Console.Write(n+" ");
+                    total++;
+                }
+                factor=2;
+            }
+            Console.WriteLine();
+            Console.WriteLine("There are {0} primes under 100.", total);
+        }
+    }
+}
